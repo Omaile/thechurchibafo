93de2b8632c91a67669f195bf73eab003f9d94fb
Omaile <mailejulius70odiase.jm@gmail.com>
2026-06-21 13:49:32 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import Hero from "../components/Hero";
-import QuickInfoBar from "../components/QuickInfoBar";
+import Hero from "./components/Hero";
+import QuickInfoBar from "./components/QuickInfoBar";
 
 export default function Home() {
   return (
     <main>
-
       <Hero />
-
       <QuickInfoBar />
-
     </main>
   );
 }
